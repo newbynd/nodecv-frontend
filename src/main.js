3c4aede9c56e16de6adb0fe1ee7f4b14ea6ca6ca
Néstor <nestorromero.contacto@gmail.com>
2020-05-01 14:20:49 +0200

    deleting bootstrap

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
